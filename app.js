c7ff93f15eb0a6a15912569946d6781b81d2f7d5
Jayson <jaysonchen64@gmail.com>
2018-03-19 18:44:05 -0500

    p and g

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
     $("#topicButtons").on("click", function(){
         var topic = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=6XPpgYRMRSDHJoXuZnglJscGARaX4Ge7&limit=10"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=6XPpgYRMRSDHJoXuZnglJscGARaX4Ge7&q="+topic+"&limit=10"
+        
         console.log(queryURL);
 
         $.ajax({
@@ -17,11 +17,14 @@ $(document).ready(function(){
                 var rating = results[i].rating;
                 var topicDiv = $("<div>");
                 var p = $("<p>").text("Rating: " + rating);
-                var topicImage = $("<img>");
-                topicImage.attr("src", results[i].images.fixed_height.url);
+                var topicImage = $("<img>").addClass("gif");
+                topicImage.attr("src", results[i].images.fixed_height.still.url);
+                topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                topicImage.attr("data-still", results[i].images.fixed_height.url);
+                topicImage.attr("data-state", "still");
                 topicDiv.append(p);
                 topicDiv.append(topicImage);
-                $("#topic").prepend(topicDiv);
+                $("#topic").append(topicDiv);
             }
         })
     })
@@ -44,5 +47,17 @@ $(document).ready(function(){
         topics.push(topic);
         renderButtons();
       });
+
+    $(document).on("click", ".gif", function(){
+        var state = $(this).attr("data-state");
+        if(state == "still"){
+            $(this).attr("src", $(this).data("animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else{
+            $(this).attr("src", $(this).data("still"));
+            $(this).attr("data-state", "still");
+        }
+    })
 
 });
